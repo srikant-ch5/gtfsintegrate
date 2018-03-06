458c9363b2716d5558d071cab59c7141510c10d1
srikant <srksriks123@gmail.com>
2018-03-06 23:59:18 +0530

    add db

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -87,6 +87,8 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
