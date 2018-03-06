8797e9ab4ff928754ee9f7a0e07e275c333d9402
srikant <srksriks123@gmail.com>
2018-03-06 23:35:43 +0530

    added

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'geodjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
