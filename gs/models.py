5fc8d81ba5247c7c4e7848c80bd83c7ae6b7c1ea
srikant <srksriks123@gmail.com>
2018-05-03 21:44:33 +0530

    add script

--- gs/models.py
+++ gs/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.gis.db import models
 
@@ -13,25 +14,13 @@ class Relation(models.Model):
 	changeset = models.IntegerField()
 
 	def __str__(self):
-		return self.rel_id
+		return " %s " %self.rel_id
 
 class Member(models.Model):
 	member_type = models.CharField(max_length=20)
 	member_ref  = models.IntegerField()
 	member_role = models.CharField(max_length=20)
 	relation    = models.ForeignKey(Relation, blank=True, null=True)
-
-class Way(models.Model):
-	way_id    = models.IntegerField(primary_key=True)
-	timestamp = models.CharField(max_length=20)
-	uid       = models.IntegerField()
-	user      = models.CharField(max_length=20)
-	version   = models.CharField(max_length=20)
-	changeset = models.IntegerField()
-	node_ref  = models.IntegerField(blank=True, null=True)
-
-	def __str__(self):
-		return self.way_id
 
 class Node(models.Model):
 	node_id   = models.IntegerField(primary_key=True)
@@ -42,17 +31,47 @@ class Node(models.Model):
 	changeset = models.IntegerField()
 	lat       = models.DecimalField(max_digits=10, decimal_places=2)
 	lon       = models.DecimalField(max_digits=10, decimal_places=2)
+	way_ref   = models.ForeignKey(Way, blank=True,null=True)
+	rel_ref   = models.ForeignKey(Relation, blank=True,null=True)
+
 	#objects = models.GeoManager()
 
 	def __str__(self):
-		return self.node_id
+		return " %s " % self.node_id
+
+class Way(models.Model):
+	way_id    = models.IntegerField(primary_key=True)
+	timestamp = models.CharField(max_length=20)
+	uid       = models.IntegerField()
+	user      = models.CharField(max_length=20)
+	version   = models.IntegerField(max_length=20)
+	changeset = models.IntegerField()
+	rel_ref  = models.ForeignKey(Relation, blank=True, null=True)
+
+	def __str__(self):
+		return " %s " % self.way_id
+
+class Key(models.Model):
+	key_id = models.IntegerField()
+	key_text = models.CharField(max_length=50)
+
+	def __str__(self):
+		return " %s " % self.key_id
+
+class Value(models.Model):
+	value_id = models.IntegerField()
+	value_text = models.CharField(max_length=50)
+
+	def __str__(self):
+		return " %s " % self.value_id
 
 class Tag(models.Model):
-	tag_id 	 = models.IntegerField()
-	key_id   = models.IntegerField()
-	value_id = models.IntegerField()
+	tag_id 	 = models.IntegerField(primary_key=True)
+	key_ref  = models.OneToOneField(Key, blank=True, null=True)
+	value_ref= models.OneToOneField(Value, blank=True, null=True)
 	node_tag = models.ForeignKey(Node, blank=True, null=True)
 	way_tag  = models.ForeignKey(Way, blank=True, null=True)
 	relation_tag = models.ForeignKey(Relation, blank=True, null=True)
+
 	def __str__(self):
-		return self.tag_id
+		return " %s " % self.tag_id
--- gs/scripts.py
+++ gs/scripts.py
@@ -1,8 +1,7 @@
 from lxml import etree
-'''import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "osm.settings")
-from models import Node,Tag,Way
-'''
+from models import Node,Tag,Way,Relation,Value,Key
+from decimal import Decimal
+
 def parseXML(osmFile):
 
     with open(osmFile) as fileobj:
@@ -15,27 +14,125 @@ def parseXML(osmFile):
     for primitive in root.getchildren():
         if primitive.tag == "node":
             snode_id   = primitive.get("id")
+            snode_id   = int(snode_id)
             stimestamp = primitive.get("timestamp")
             suid       = primitive.get("uid")
+            suid       = int(suid)
             suser      = primitive.get("user")
             sversion   = primitive.get("version")
+            sversion   = int(sversion)
             schangeset = primitive.get("changeset")
+            schangeest = int(schangeset)
             slat       = primitive.get("lat")
+            slat       = Decimal(slat)
             slon       = primitive.get("lon")
+            slon       = Decimal(slon)
 
             node = Node(node_id=snode_id, timestamp=stimestamp,uid=suid,user=suser,version=sversion,changeset=schangeset,lat=slat,lon=slon)
             node.save()
-            
-            for tag in primitive.getchildren():
+            for xmlTag in primitive.getchildren():
                 stag_id = tag_counter
-                skey = tag.get("key")
-                svalue = tag.get("value")
-                tag_counter += tag_counter
+                skey = xmlTag.get("key")
+                svalue = xmlTag.get("value")
 
-                tag = Tag(tag_id=stag_id,key=skey,value=svalue,node_tag=node)
+                key = Key(key_id=keyid_counter,key_text=skey)
+                key.save()
+                value = Value(value_id=valueid_counter,value_text=svalue)
+                value.save()
+
+                tag_counter += tag_counter
+                keyid_counter += keyid_counter
+                valueid_counter += valueid_counter
+
+                tag = Tag(tag_id=stag_id,key_ref=key,value_ref=value,node_tag=node)
                 tag.save()
-        else:
-            break
+                node.tag_set.add(tag)
+                node.save()
+
+        elif primitive.tag == "way":
+            #w prefix refers single way
+            wway_id    = int(primitive.get("id"))
+            wtimestamp = primitive.get("timestamp")
+            wuid       = int(primitive.get("uid"))
+            wuser      = primitive.get("user")
+            wversion   = int(primitive.get("version"))
+            wchangeset = int(primitive.get("changeset"))
+            
+            way = Way(way_id=wway_id,timestamp=wtimestamp,uid=wuid,user=wuser,version=wversion,changeset=wchangeset)
+            way.save()
+
+            for xmlTag in primitive.getchildren():
+                if xmlTag.tag == "nd":
+                    #reference for node
+                    nodeReference = int(xmlTag.get("ref"))
+                    node_in_way  = Node.objects.filter(node_id=nodeReference)
+                    way.node_set.add(node_in_way)
+                    way.save()
+
+                elif xmlTag.tag == "tag":
+                    wtag_id = tag_counter
+                    wkey    = xmlTag.get("key")
+                    wvalue  = xmlTag.get("value")
+
+                    key = Key(key_id=keyid_counter,key_text=wkey)
+                    key.save()
+                    value = Value(value_id = valueid_counter,value_text=wvalue)
+                    value.save()
+
+                    tag_counter += tag_counter
+                    keyid_counter += keyid_counter
+                    valueid_counter += valueid_counter
+
+                    tag = Tag(tag_id=stag_id,key_ref=key,value_ref=value,node_tag=node)
+                    tag.save()
+                    way.tag_set.add(tag)
+                    way.save()
+
+        elif primitive.tag == "relation":
+            rrelation_id    = int(primitive.get("id"))
+            rtimestamp = primitive.get("timestamp")
+            ruid       = int(primitive.get("uid"))
+            ruser      = primitive.get("user")
+            rversion   = int(primitive.get("version"))
+            rchangeset = int(primitive.get("changeset"))
+            
+            relation = Relation(relation_id=rrelation_id,timestamp=rtimestamp,uid=ruid,user=ruser,version=rversion,changeset=rchangeset)
+            relation.save()
+
+            if primitive.tag == 'member':
+
+                for xmlTag in primitive.getchildren():
+
+                    if xmlTag.tag == "member":
+                        mem_type = xmlTag.get("type")
+                        mem_ref = int(xmlTag.get("ref"))
+
+                        if mem_type == "node":
+                            node = Node.objects.filter(node_id=mem_ref)
+                            relation.node_set.add(node)
+                        elif mem_type == "way":
+                            way = Way.objects.filter(way_id=mem_ref)
+                            relation.way_set.add(way)
+
+                        relation.save()
+                    elif xmlTag.tag == "tag":
+                        rtag_id = tag_counter
+                        rkey    = xmlTag.get("key")
+                        rvalue  = xmlTag.get("value")
+
+                        key = Key(key_id=keyid_counter,key_text=rkey)
+                        key.save()
+                        value = Value(value_id = valueid_counter,value_text=rvalue)
+                        value.save()
+
+                        tag_counter += tag_counter
+                        keyid_counter += keyid_counter
+                        valueid_counter += valueid_counter
+
+                        tag = Tag(tag_id=stag_id,key_ref=key,value_ref=value,node_tag=node)
+                        tag.save()
+                        relation.tag_set.add(tag)
+                        relation.save()
 
 if __name__ == "__main__":
     parseXML("PTsamples.xml")
