412ab6dedf0fe12cd1c83af54f7a7f391a230db3
srikant <srksriks123@gmail.com>
2018-05-07 01:21:18 +0530

    Update scripts.py

--- gs/scripts.py
+++ gs/scripts.py
@@ -1,45 +1,7 @@
 from django.shortcuts import render
 from lxml import etree
-from .models import Node,Tag,Way,Relation,Value,Key
+from .models import Node,Tag,Way,Relation,Value,Key,Member
 from decimal import Decimal
-
-def storetoDb(request):
-
-  #node = Node(node_id=45455454513586, timestamp='2018-01-15T17:26:05Z',uid=72151,user='GeorgFausB',version=13,changeset=55469382,lat=50.9558026,lon=6.9691347)
-  #node.save()
-  '''
-  test_key = Key(key_id=995123,key_text="test_key")
-  test_value =Value(value_id=239122,value_text="test_value")
-  test_key.save()
-  test_value.save()
-  test_tag = Tag(tag_id=9848422595)
-  test_tag.save()
-  test_tag.key_ref.add(test_key)
-  test_tag.value_ref.add(test_value)
-  
-  t = Tag.objects.get(tag_id=9848422595)
-  t.key_ref.all().values_list('key_text',flat=True)
-  '''
-  #node2 = Node(node_id=234235424544545,timestamp='234tz',uid=234234,user='Srikant',version=1,changeset=23,lat=56.67,lon=6.889,tag_ref=tag1)
-  #node2.save()
-
-  t = Tag.objects.all()
-  flag= 0
-  key_to_search = 'test_key'
-  value_to_search = 'test_value'
-
-  for onetag in t:
-    for i in range(0,onetag.key_ref.count()):
-      print ("%s   %s" %(onetag.key_ref.all()[i].key_text,onetag.value_ref.all()[i]))
-      if onetag.key_ref.all()[i].key_text == key_to_search and onetag.value_ref.all()[i].value_text == value_to_search:
-        flag = 1
-        break
-
-  if flag == 1:
-      print("Found")
-  else:
-      print("Not found")
-  return render(request,'osm/confirm.html')
 
 def storeIntoDb(request):
 
@@ -51,11 +13,12 @@ def storeIntoDb(request):
 
   root = etree.fromstring(xmlfile,parser=parser)
 
-  tag_counter = 10000
-  key_counter = 5000
-  value_counter = 5000
+  tag_counter = 102342787685
+  key_counter = 5234376878786
+  value_counter = 6323467675642
 
   for primitive in root.getchildren():
+
       if primitive.tag == "node":
           snode_id   = int(primitive.get("id"))
           stimestamp = primitive.get("timestamp")
@@ -81,21 +44,23 @@ def storeIntoDb(request):
             flag= 0
             key_to_search = get_key_text_from_xml
             value_to_search = get_value_text_from_xml
+            tag_to_use = Tag()
+            tag_index = 0
 
             for onetag in t:
-              for i in range(0,onetag.key_ref.count()):
-                #print ("%s   %s" %(onetag.key_ref.all()[i].key_text,onetag.value_ref.all()[i]))
-                if onetag.key_ref.all()[i].key_text == key_to_search and onetag.value_ref.all()[i].value_text == value_to_search:
-                  flag = 1
-                  break
+              if onetag.key_ref.all()[0].key_text == key_to_search and onetag.value_ref.all()[0].value_text == value_to_search:
+                flag = 1
+                tag_index = onetag.tag_id
+                break
 
             if flag == 1:
-                print("Found")
-                tag_to_use = Tag.objects.filter(tag_id=onetag.tag_id)[0]
-                #return the tag as it is already in the DB
+                tag_to_use = Tag.objects.get(tag_id=tag_index)
+                node.tag_ref.add(tag_to_use)
+                node.save()
 
             else:
                 #create a key value with text and id counter and also create a Tag using them
+                print("creating new tag")
                 key_count = Key.objects.filter(key_text=key_to_search).count()
 
                 if key_count == 0:
@@ -106,9 +71,9 @@ def storeIntoDb(request):
                   countDec = key_count
                   while countDec > 1:
                     Key.objects.filter(key_text=key_to_search)[0].delete()
+                    print("Deleted one similar key")
                     countDec -= 1
-                unique_key = Key.objects.get(key_text=key_to_search)
-                unique_key.save()
+
                 value_count = Value.objects.filter(value_text=value_to_search).count()
 
                 if value_count == 0:
@@ -121,105 +86,214 @@ def storeIntoDb(request):
                     Value.objects.filter(value_text=value_to_search)[0].delete()
                     countVDec -= 1
 
-                unique_value =Value.objects.filter(value_text=value_to_search)
-                unique_value.save()
+                unique_key = Key.objects.get(key_text=key_to_search)
+                unique_value =Value.objects.get(value_text=value_to_search)
 
-                tag = Tag(tag_id=tag_counter,key_ref=unique_key,value_ref=unique_value)
-                tag.save()
-                tag_to_use = Tag.objects.filter(tag_id=tag_counter)
+                tag_to_create = Tag(tag_id = 78934556)
+                tag_to_create.save()
+                tag_to_create.key_ref.add(unique_key)
+                tag_to_create.value_ref.add(unique_value)
+                tag_to_create.save()
+                tag_to_use = Tag.objects.filter(tag_id=78934556)
+                print(tag_to_use)
                 tag_counter += 1
-
-            #print("Tag %d (%d %s)(%d %s)" %(tag_counter,keyid_counter,skey,valueid_counter,svalue))
-          node_to_add_tag = Node.objects.get(node_id=snode_id)
-          node_to_add_tag.tag_ref.add(tag_to_use)
-          node_to_add_tag.save()
-      '''
-      elif primitive.tag == "way":
-          #w prefix refers single way
-          wway_id    = int(primitive.get("id"))
-          wtimestamp = primitive.get("timestamp")
-          wuid       = int(primitive.get("uid"))
-          wuser      = primitive.get("user")
-          wversion   = int(primitive.get("version"))
-          wchangeset = int(primitive.get("changeset"))
-          
-          way = Way(way_id=wway_id,timestamp=wtimestamp,uid=wuid,user=wuser,version=wversion,changeset=wchangeset)
-          way.save()
-
-          for xmlTag in primitive.getchildren():
-              if xmlTag.tag == "nd":
-
-                  nodeReference = int(xmlTag.get("ref"))
-                  node_in_way  = Node.objects.filter(node_id=nodeReference)[0]
-                  way.node_set.add(node_in_way)
-                  way.save()
-
-              elif xmlTag.tag == "tag":
-                  wtag_id = tag_counter
-                  wkey    = xmlTag.get("k")
-                  wvalue  = xmlTag.get("v")
-
-                  #print("Key %d %s" %(keyid_counter,skey))
-                  #print("Value %d %s " %(valueid_counter,svalue))
-                  key = Key(key_id=keyid_counter,key_text=wkey)
-                  key.save()
-                  value = Value(value_id = valueid_counter,value_text=wvalue)
-                  value.save()
                 
-                  tag_counter += 1
-                  keyid_counter += 1
-                  valueid_counter += 1
+                node = Node.objects.get(node_id=snode_id)
+                node.tag_ref.add(tag_to_use)
+                node.save()
 
-                  print("Tag %d (%d %s)(%d %s)" %(tag_counter,keyid_counter,wkey,valueid_counter,wvalue))
-                  tag = Tag(tag_id=stag_id,key_ref=key,value_ref=value,node_tag=node)
-                  tag.save()
-                  way.tag_set.add(tag)
-                  way.save()
+      elif primitive.tag == 'way':
+
+        wway_id    = int(primitive.get("id"))
+        wtimestamp = primitive.get("timestamp")
+        wuid       = int(primitive.get("uid"))
+        wuser      = primitive.get("user")
+        wversion   = int(primitive.get("version"))
+        wchangeset = int(primitive.get("changeset"))
+
+        way = Way(way_id=wway_id,timestamp=wtimestamp,uid=wuid,user=wuser,version=wversion,changeset=wchangeset)
+        way.save()
+
+        for xmlTag in primitive.getchildren():
+
+          if xmlTag.tag == 'nd':
+            node_reference = int(xmlTag.get("ref"))
+            node_to_add = Node.objects.get(node_id=node_reference)
+            way.node_ref.add(node_to_add)
+            way.save()
+
+          elif xmlTag.tag == 'tag':
+
+            get_key_text_from_xml = xmlTag.get("k")
+            get_value_text_from_xml = xmlTag.get("v")
+
+            #step 2 is to check if the tag with same key and value exists
+            t = Tag.objects.all()
+            flag= 0
+            key_to_search = get_key_text_from_xml
+            value_to_search = get_value_text_from_xml
+            tag_to_use = Tag()
+            tag_index = 0
+
+            for onetag in t:
+              if onetag.key_ref.all()[0].key_text == key_to_search and onetag.value_ref.all()[0].value_text == value_to_search:
+                flag = 1
+                tag_index = onetag.tag_id
+                break
+
+            if flag == 1:
+                tag_to_use = Tag.objects.get(tag_id=tag_index)
+                way.tag_ref.add(tag_to_use)
+                way.save()
+
+            else:
+                #create a key value with text and id counter and also create a Tag using them
+                print("creating new tag")
+                key_count = Key.objects.filter(key_text=key_to_search).count()
+
+                if key_count == 0:
+                  key = Key(key_id=key_counter,key_text=key_to_search)
+                  key.save()
+                  key_counter += 1
+                elif key_count >1:
+                  countDec = key_count
+                  while countDec > 1:
+                    Key.objects.filter(key_text=key_to_search)[0].delete()
+                    print("Deleted one similar key")
+                    countDec -= 1
+
+                value_count = Value.objects.filter(value_text=value_to_search).count()
+
+                if value_count == 0:
+                  value = Value(value_id=value_counter, value_text=value_to_search)
+                  value.save()
+                  value_counter += 1
+                elif value_count > 1:
+                  countVDec = value_count
+                  while countVDec > 1:
+                    Value.objects.filter(value_text=value_to_search)[0].delete()
+                    countVDec -= 1
+
+                unique_key = Key.objects.get(key_text=key_to_search)
+                unique_value =Value.objects.get(value_text=value_to_search)
+
+                tag_to_create = Tag(tag_id = tag_counter)
+                tag_to_create.save()
+                tag_to_create.key_ref.add(unique_key)
+                tag_to_create.value_ref.add(unique_value)
+                tag_to_create.save()
+                tag_to_use = Tag.objects.get(tag_id=tag_counter)
+                print(tag_to_use)
+                tag_counter += 1
+                
+                way = Way.objects.get(way_id=wway_id)
+                way.tag_ref.add(tag_to_use)
+                way.save()
 
       elif primitive.tag == "relation":
-          rrelation_id    = int(primitive.get("id"))
-          rtimestamp = primitive.get("timestamp")
-          ruid       = int(primitive.get("uid"))
-          ruser      = primitive.get("user")
-          rversion   = int(primitive.get("version"))
-          rchangeset = int(primitive.get("changeset"))
-          
-          #print("         Relation   %d %s %d %s %d %d" %(rrelation_id,rtimestamp,ruid,ruser,rversion,rchangeset))
-          relation = Relation(relation_id=rrelation_id,timestamp=rtimestamp,uid=ruid,user=ruser,version=rversion,changeset=rchangeset)
-          relation.save()
 
-          for xmlTag in primitive.getchildren():
+        rrelation_id  = int(primitive.get("id"))
+        rtimestamp    = primitive.get("timestamp")
+        ruid          = int(primitive.get("uid"))
+        ruser         = primitive.get("user")
+        rversion      = int(primitive.get("version"))
+        rchangeset    = int(primitive.get("changeset"))
 
-              if xmlTag.tag == "member":
-                  mem_type = xmlTag.get("type")
-                  mem_ref = int(xmlTag.get("ref"))
+        relation = Relation(rel_id=rrelation_id,timestamp=rtimestamp,uid=ruid,user=ruser,version=rversion,changeset=rchangeset)
+        relation.save()
 
-                  if mem_type == "node":
-                      node = Node.objects.filter(node_id=mem_ref)
-                      relation.node_set.add(node)
-                  elif mem_type == "way":
-                      way = Way.objects.filter(way_id=mem_ref)
-                      relation.way_set.add(way)
-  
-                  relation.save()
-              elif xmlTag.tag == "tag":
-                  rtag_id = tag_counter
-                  rkey    = xmlTag.get("k")
-                  rvalue  = xmlTag.get("v")
+        for xmlTag in primitive.getchildren():
 
-                  key = Key(key_id=keyid_counter,key_text=rkey)
+
+          if xmlTag.tag == "member":
+
+            mem_type = xmlTag.get("type")
+            mem_ref = int(xmlTag.get("ref"))
+            mem_role = xmlTag.get("role")
+            member = Member(member_type=mem_type,member_role=mem_role)
+            member.save()
+
+            if mem_type == "node":
+                print(mem_ref)
+                node_refering_relation = Node.objects.get(node_id=mem_ref)
+                print(node_refering_relation)
+                member.node_ref.add(node_refering_relation)
+
+            elif mem_type == "way":
+                way = Way.objects.get(way_id=mem_ref)
+                member.way_ref.add(way)
+
+            relation = Relation(relation_id=rrelation_id)
+            relation.mem_ref.add(member)
+            relation.save()
+
+          elif xmlTag.tag == "tag":
+
+            get_key_text_from_xml = xmlTag.get("k")
+            get_value_text_from_xml = xmlTag.get("v")
+
+            #step 2 is to check if the tag with same key and value exists
+            t = Tag.objects.all()
+            flag= 0
+            key_to_search = get_key_text_from_xml
+            value_to_search = get_value_text_from_xml
+            tag_to_use = Tag()
+            tag_index = 0
+
+            for onetag in t:
+              if onetag.key_ref.all()[0].key_text == key_to_search and onetag.value_ref.all()[0].value_text == value_to_search:
+                flag = 1
+                tag_index = onetag.tag_id
+                break
+
+            if flag == 1:
+                tag_to_use = Tag.objects.get(tag_id=tag_index)
+                relation.tag_ref.add(tag_to_use)
+                relation.save()
+
+            else:
+                #create a key value with text and id counter and also create a Tag using them
+                print("creating new tag")
+                key_count = Key.objects.filter(key_text=key_to_search).count()
+
+                if key_count == 0:
+                  key = Key(key_id=key_counter,key_text=key_to_search)
                   key.save()
-                  value = Value(value_id = valueid_counter,value_text=rvalue)
-                  value.save()
+                  key_counter += 1
+                elif key_count >1:
+                  countDec = key_count
+                  while countDec > 1:
+                    Key.objects.filter(key_text=key_to_search)[0].delete()
+                    print("Deleted one similar key")
+                    countDec -= 1
 
-                  tag_counter += 1
-                  keyid_counter += 1
-                  valueid_counter += 1
-                  #print("Tag %d (%d %s)(%d %s)" %(tag_counter,keyid_counter,rkey,valueid_counter,rvalue))
-                  
-                  tag = Tag(tag_id=stag_id,key_ref=key,value_ref=value,node_tag=node)
-                  tag.save()
-                  relation.tag_set.add(tag)
-                  relation.save()'''
+                value_count = Value.objects.filter(value_text=value_to_search).count()
+
+                if value_count == 0:
+                  value = Value(value_id=value_counter, value_text=value_to_search)
+                  value.save()
+                  value_counter += 1
+                elif value_count > 1:
+                  countVDec = value_count
+                  while countVDec > 1:
+                    Value.objects.filter(value_text=value_to_search)[0].delete()
+                    countVDec -= 1
+
+                unique_key = Key.objects.get(key_text=key_to_search)
+                unique_value =Value.objects.get(value_text=value_to_search)
+
+                tag_to_create = Tag(tag_id = tag_counter)
+                tag_to_create.save()
+                tag_to_create.key_ref.add(unique_key)
+                tag_to_create.value_ref.add(unique_value)
+                tag_to_create.save()
+                tag_to_use = Tag.objects.get(tag_id=tag_counter)
+                print(tag_to_use)
+                tag_counter += 1
+                
+                relation = relation.objects.get(rel_id=rrel_id)
+                relation.tag_ref.add(tag_to_use)
+                relation.save()
+
 
   return render(request,'osm/confirm.html')
