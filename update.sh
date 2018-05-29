234ebf133f2cde82cb1474979544a3d4b6fd36d9
PolyglotOpenstreetmap <winfixit@gmail.com>
2018-05-29 22:44:28 +0200

    requirements.txt updated, also changed update.sh script so it only installs from requirements if that file was changed less than 1 hour before.

--- update.sh
+++ update.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+#funtion arguments -> filename to compare against curr time
+function comparedate() {
+if [ ! -f $1 ]; then
+  echo "file $1 does not exist"
+        exit 1
+fi
+MAXAGE=$(bc <<< '1*60*60') # seconds in 1 hour
+# file age in seconds = current_time - file_modification_time.
+FILEAGE=$(($(date +%s) - $(stat -c '%Y' "$1")))
+test $FILEAGE -lt $MAXAGE && {
+    echo "$1 is less than 28 hours old."
+    return 0
+}
+echo "$1 is older than 28 hours seconds."
+return 1
+}   
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -8,8 +25,14 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
     echo "Repositories are up-to-date"
 elif [ $LOCAL = $BASE ]; then
+    # pull latest version from github if there is one, abort when that fails
     git pull || { echo 'pulling from github failed' ; exit 1; }
-    pip install -r requirements.txt
+    # only pip install from requirements.txt, when file was modified in last hour
+    requirements=requirements.txt
+    if comparedate $requirements
+    then
+        pip install -r $requirements
+    fi
     python manage.py makemigrations --merge
     python manage.py makemigrations
     python manage.py migrate
