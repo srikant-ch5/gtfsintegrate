28ac34ce240775c2351e00d079095afba10df995
sriks123 <srksriks123@gmail.com>
2018-06-06 17:30:32 +0530

    Zoom Leaflet map to the bounds of the feed stops

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -45,6 +45,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages', ],
         },
+    }
 ]
 
 WSGI_APPLICATION = 'geodjango.wsgi.application'
