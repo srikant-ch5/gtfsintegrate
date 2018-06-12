0bda72f4fabaf262e9ed3eeb9c87c146f4b169b6
dkocich <dkocich@users.noreply.github.com>
2018-06-12 21:45:21 +0200

    chore(scripts): add test db script; install geos/gdal packages

--- scripts/create_test_db.sh
+++ scripts/create_test_db.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 sudo -u postgres psql <<EOF
-DROP DATABASE IF EXISTS $1_gtfsintegrate;
+DROP DATABASE IF EXISTS $1_gtfsintegrate_test;
 DROP USER IF EXISTS $1;
 CREATE USER $1 WITH PASSWORD 'password';
-CREATE DATABASE $1_gtfsintegrate OWNER $1;
+CREATE DATABASE $1_gtfsintegrate_test OWNER $1;
 \c postgres
 CREATE EXTENSION IF NOT EXISTS adminpack;
-\c $1_gtfsintegrate
+\c $1_gtfsintegrate_test
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
 \q
