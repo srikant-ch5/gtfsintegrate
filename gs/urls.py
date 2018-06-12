605959999a0166a1f6eb91003c6628d224cd5af1
sriks123 <srksriks123@gmail.com>
2018-06-13 03:49:03 +0530

    Show labels on markers in map

--- gs/urls.py
+++ gs/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^latest-feed/', osmview.load, name="latest-feed"),
     url(r'^nodedata/', GeoJSONLayerView.as_view(model=Node, properties=('id')), name="nodedata"),
-    url(r'^stopdata/', GeoJSONLayerView.as_view(model=Stop, properties=('id', 'feed')), name="stopdata"),
+    url(r'^stopdata/', GeoJSONLayerView.as_view(model=Stop, properties=('id', 'feed','name')), name="stopdata"),
     url(r'^gtfsstopdata/(?P<pk>\d+)/$', GTFS_Stop_View.as_view(), name="gtfsstopdata"),
     # url(r'^waydata/', GeoJSONLayerView.as_view(model=Way, properties=('id','version','visible','incomplete')), name="waydata"),
     url(r'^route_masters', osmview.get_route_master_relations, name="route_master"),
