247bdbc0f2ddb2abd9e68d9a4909a7e8aea49e3a
sriks123 <srksriks123@gmail.com>
2018-06-13 01:50:50 +0530

    Add loader to show feed loading and displaying process

--- gs/urls.py
+++ gs/urls.py
@@ -15,7 +15,7 @@ import compare.views as compview
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-    url(r'^osm/', osmview.load, name="osm"),
+    url(r'^latest-feed/', osmview.load, name="latest-feed"),
     url(r'^nodedata/', GeoJSONLayerView.as_view(model=Node, properties=('id')), name="nodedata"),
     url(r'^stopdata/', GeoJSONLayerView.as_view(model=Stop, properties=('id', 'feed')), name="stopdata"),
     url(r'^gtfsstopdata/(?P<pk>\d+)/$', GTFS_Stop_View.as_view(), name="gtfsstopdata"),
