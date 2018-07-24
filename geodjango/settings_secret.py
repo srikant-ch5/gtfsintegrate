15125c87389cf27c891adc8ceddd01366203d92e
sriks123 <srksriks123@gmail.com>
2018-07-25 01:24:38 +0530

    Add a new OSM node with name and ref tag in JOSM

--- geodjango/settings_secret.py
+++ geodjango/settings_secret.py
@@ -1,1 +1,16 @@
+SECRET_KEY = '5@ch@sqd_+(4eaj2h60qofszfhuuxk#h#f#ehyb&b+drp@v0&s'
 
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gtfsintegratedb4',
+        'USER': 'sriks',
+        'PASSWORD': 'sriks@123',
+        'HOST': 'localhost',  # '127.0.0.1' probably works also
+        'PORT': '5432',
+    }
+}
